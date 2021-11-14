eebf96f71e8cc835105201aa9cb8993151f3ba7f
Lohit Paul <71273110+Kalashnikov-cmd@users.noreply.github.com>
2021-11-14 17:41:33 +0530

    Update mac_address.py

--- mac_address.py
+++ mac_address.py
@@ -2,10 +2,6 @@
 
 import subprocess
 
-#interface = input("Enter interface:")
-
-#mac = input("Enter new MAC address:")
-
 print("Available interfaces and its corresponding MAC addresses:\n")
 subprocess.call("ifconfig | sed -En -rn '/^[^[:space:]]/p; /ether/p'", shell=True)
 print("")
@@ -16,4 +12,3 @@ mac = input("Enter new MAC Address:")
 subprocess.call("sudo ifconfig " + interface + " down", shell=True)
 subprocess.call("sudo ifconfig " + interface + " hw ether " + mac, shell=True)
 subprocess.call("sudo ifconfig " + interface + " up", shell=True)
-
