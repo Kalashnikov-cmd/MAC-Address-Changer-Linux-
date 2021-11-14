9e2773766a97feb63881bb6c02efde7199bd1db2
Lohit Paul <71273110+Kalashnikov-cmd@users.noreply.github.com>
2021-11-14 17:37:43 +0530

    Create mac_address.py

---
+++ mac_address.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import subprocess
+
+#interface = input("Enter interface:")
+
+#mac = input("Enter new MAC address:")
+
+print("Available interfaces and its corresponding MAC addresses:\n")
+subprocess.call("ifconfig | sed -En -rn '/^[^[:space:]]/p; /ether/p'", shell=True)
+print("")
+
+interface = input("Enter the interface:")
+mac = input("Enter new MAC Address:")
+
+subprocess.call("sudo ifconfig " + interface + " down", shell=True)
+subprocess.call("sudo ifconfig " + interface + " hw ether " + mac, shell=True)
+subprocess.call("sudo ifconfig " + interface + " up", shell=True)
