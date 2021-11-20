2d6c14333d5a359a33a56b9279a574bbb8debc84
Lohit Paul <71273110+Kalashnikov-cmd@users.noreply.github.com>
2021-11-20 20:31:33 +0530

    Update mac_address.py

--- mac_address.py
+++ mac_address.py
@@ -13,4 +13,4 @@ subprocess.call(["sudo", "ifconfig", interface, "down"])
 subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", mac])
 subprocess.call(["sudo", "ifconfig", interface, "up"])
 
-print("[+] Changed MAC Address for " + interface " to " + mac)
+print("[+] Changed MAC Address for " + interface + " to " + mac)
